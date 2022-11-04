3c257d16da4af8280e6b3e243ae6f1104aac5004
Andres Zapata Rodriguez <andres.zapata-rodriguez@tum.de>
2022-11-04 15:33:58 +0100

    Initial commit

---
+++ src/gym.py
@@ -0,0 +1,35 @@
+import gym
+import numpy as np
+
+from gym import spaces
+from dataclasses import dataclass
+
+import logging
+dataclass
+class GymParams():
+    pass
+
+@dataclass
+class CPPGymParams(GymParams):
+    pass
+
+class Gym():
+    def __int__(self):
+        pass
+    def reset(self):
+        pass
+    def step(self, action):
+        pass
+    def _get_rewards(self):
+        pass
+    def _reset(self):
+        pass
+    def _get_obs(self):
+        pass
+    def _get_info(self):
+        pass
+    def render(self):
+        pass
+    def _render_frame(self):
+        pass
+
